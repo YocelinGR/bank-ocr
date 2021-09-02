9150d331b12709acf77b52c9f463937f9878a9ba
Yocelin Garcia <garcia_romero.y@hotmail.com>
2021-09-02 01:58:34 -0500

    Files manager init module

--- lib/files/file_manager.ex
+++ lib/files/file_manager.ex
@@ -2,4 +2,20 @@ defmodule BankOcr.Files.FileManager do
   @moduledoc """
   This module will manage the operations related to manipulate files like: read, write, manage its content, etc
   """
+  @doc """
+  Read File
+
+  ## Examples
+
+      iex> BankOcr.Files.FileManager.readFile("input_file.txt")
+      {:ok, file_content} | {:err, "File does not have content"}
+
+  """
+  @spec readFile(String.t()) :: {:ok, String.t()} |{:err, String.t()}
+  def readFile(file_name) do
+    case File.read(file_name) do
+      {:ok, contents} -> {:ok, contents}
+      {:error, :enoent} -> {:err, "File does not have content"}
+    end
+  end
 end
