1f16d88dee37e016bf66e543a49fd301ebe067d2
Yocelin Garcia Romero <garcia_romero.y@hotmail.com>
2021-09-06 19:08:58 -0500

    Add the fn to write files and move files to write and read to main path

--- lib/files/file_manager.ex
+++ lib/files/file_manager.ex
@@ -2,20 +2,36 @@ defmodule BankOcr.Files.FileManager do
   @moduledoc """
   This module will manage the operations related to manipulate files like: read, write, manage its content, etc
   """
+  require Logger
   @doc """
   Read File
-
   ## Examples
 
       iex> BankOcr.Files.FileManager.readFile("input_file.txt")
-      {:ok, file_content} | {:err, "File does not have content"}
+      {:ok, file_content} | {:err, "Couldn't find that file you were looking for"}
 
   """
   @spec readFile(String.t()) :: {:ok, String.t()} |{:err, String.t()}
   def readFile(file_name) do
     case File.read(file_name) do
       {:ok, contents} -> {:ok, contents}
-      {:error, :enoent} -> {:err, "File does not have content"}
+      {:error, :enoent} -> {:err, "Couldn't find that file you were looking for"}
+    end
+  end
+
+  @doc """
+  Write File
+  ## Examples
+
+      iex> BankOcr.Files.FileManager.writeFile("file_processed.txt", "content")
+      {:ok, "File writed"} | {:err, "Ops, something happend"}
+
+  """
+  @spec writeFile(String.t(), String.t()) :: {:ok, String.t()} |{:err, String.t()}
+  def writeFile(file_name, content) do
+    case File.write(file_name, content) do
+      :ok -> {:ok, "File writed"}
+      _ -> {:err, "Ops, something happend"}
     end
   end
 end
