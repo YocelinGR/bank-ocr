d08f12132e44a75aea2f78d82f2657f97a3e8b98
Yocelin Garcia Romero <garcia_romero.y@hotmail.com>
2021-09-06 21:19:28 -0500

    Fix input file in order to have correct output

--- lib/files/file_manager.ex
+++ lib/files/file_manager.ex
@@ -2,7 +2,7 @@ defmodule BankOcr.Files.FileManager do
   @moduledoc """
   This module will manage the operations related to manipulate files like: read, write, manage its content, etc
   """
-  require Logger
+
   @doc """
   Read File
   ## Examples
